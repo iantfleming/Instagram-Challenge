4018df92bf96b4d8e700a5a4fa7b250be1c70e4a
Ian Fleming <>
2021-01-30 19:57:24 +0000

    renamed user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "homepage#index"
+  root "user#index"
 
-  get "/homepage", to: 'homepage#index'
+  get "/user", to: 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
